59f26b92eb0bc5715dbfed65dc0c59043c93abd0
Lautaro González <lautarong796@gmail.com>
2021-03-07 19:42:31 -0300

    add positions view

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,23 +1,18 @@
 <script>
-	import { Router, Link, Route } from "svelte-routing";
-	import Positions from "./pages/Positions.svelte";
+    import {Router, Link, Route} from "svelte-routing";
+    import Positions from "./pages/Positions.svelte";
 
-	export let url = "";
+    export let url = "";
 </script>
 
 <Router url="{url}">
-	<nav>
-		<Link to="/">Home</Link>
-		<Link to="about">About</Link>
-		<Link to="blog">Blog</Link>
-	</nav>
-	<div>
-		<Route path="/"><Positions /></Route>
-	</div>
+    <Route path="/positions">
+        <Positions/>
+    </Route>
 </Router>
 
 <style global>
-	@tailwind base;
-	@tailwind components;
-	@tailwind utilities;
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
 </style>
--- frontend/src/pages/Positions.svelte
+++ frontend/src/pages/Positions.svelte
@@ -1,7 +1,102 @@
 <script>
 </script>
 
-<main>
-    <h1 class="text-sm">Hello s!</h1>
-    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<!-- component -->
+<div class="container mx-auto px-4 sm:px-8">
+    <div class="py-8">
+        <div>
+            <h2 class="text-2xl font-semibold leading-tight">Ranking</h2>
+        </div>
+        <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                <table class="min-w-full leading-normal">
+                    <thead>
+                    <tr>
+                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Posición
+                        </th>
+                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Jugador
+                        </th>
+                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Clásico
+                        </th>
+                        <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Rápido
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2">
+                            <p class="text-gray-900 whitespace-no-wrap text-center"># 1</p>
+                        </td>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p class="text-gray-900 whitespace-no-wrap text-center">Lautaro González (Gonza)</p>
+                        </td>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p class="text-gray-900 whitespace-no-wrap text-center">
+                                <b>
+                                    <span class="text-yellow-500">Gauchito</span> 775 PL
+                                </b>
+                            </p>
+                        </td>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p class="text-gray-900 whitespace-no-wrap text-center">
+                                <b>
+                                    <span class="text-red-500">Picante</span> 1775 PL
+                                </b>
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2">
+                            <p class="text-gray-900 whitespace-no-wrap text-center"># 2</p>
+                        </td>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p class="text-gray-900 whitespace-no-wrap text-center">Eduardo Culasso (Bomber)</p>
+                        </td>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p class="text-gray-900 whitespace-no-wrap text-center">
+                            <p class="text-gray-900 whitespace-no-wrap text-center">
+                                <b>
+                                    <span class="text-yellow-500">Gauchito</span> 775 PL
+                                </b>
+                            </p>
+                        </td>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p class="text-gray-900 whitespace-no-wrap text-center">
+                                <b>
+                                    <span class="text-red-500">Picante</span> 1775 PL
+                                </b>
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2">
+                            <p class="text-gray-900 whitespace-no-wrap text-center"># 3</p>
+                        </td>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p class="text-gray-900 whitespace-no-wrap text-center">Marcelo Taborda (Beto)</p>
+                        </td>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p class="text-gray-900 whitespace-no-wrap text-center">
+                                <b>
+                                    <span class="text-yellow-500">Gauchito</span> 775 PL
+                                </b>
+                            </p>
+                        </td>
+                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p class="text-gray-900 whitespace-no-wrap text-center">
+                                <b>
+                                    <span class="text-red-500">Picante</span> 1775 PL
+                                </b>
+                            </p>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
